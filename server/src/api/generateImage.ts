cc6cde498c0341e4a6c6d73b1ff7b66d562fe4c0
Neel <neelnarayanshetty@gmail.com>
2023-05-08 19:21:38 +0530

    number of images input

--- server/src/api/generateImage.ts
+++ server/src/api/generateImage.ts
@@ -13,6 +13,7 @@ export async function generateImage(req: Request, res: Response) {
     color: string;
     prompt: string;
     style: string;
+    numberOfIcons: string;
   };
 
   const input: inputType = req.body;
@@ -22,12 +23,16 @@ export async function generateImage(req: Request, res: Response) {
     res.json(base64Image);
     return;
   } else {
-    const response = await openai.createImage({
-      prompt: `a modern ${input.shape} icon in ${input.color} of ${input.prompt}, ${input.style}, minimialistic, high quality, trending on art station, unreal engine graphics quality`,
-      n: 1,
-      size: "512x512",
-      response_format: "b64_json",
-    });
-    return res.json(response.data.data[0].b64_json);
+    try {
+      const response = await openai.createImage({
+        prompt: `a modern ${input.shape} icon in ${input.color} of ${input.prompt}, ${input.style}, minimialistic, high quality, trending on art station, unreal engine graphics quality`,
+        n: input.numberOfIcons ? parseInt(input.numberOfIcons, 10) : 1,
+        size: "512x512",
+        response_format: "b64_json",
+      });
+      return res.json({ images: response.data.data });
+    } catch (err) {
+      return res.json({ status: 418, error: JSON.stringify(err) });
+    }
   }
 }
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,10 @@
 import express from "express";
-import bodyParser from "body-parser";
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import { generateImage } from "./api/generateImage";
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT ? process.env.PORT : 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
